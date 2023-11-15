36d83462fb6a19626563121cdb32e2d2c0efdb41
Gabriel Horstmann <gabriel.ah@hotmail.com>
2023-11-15 16:05:48 -0300

    update mensagem error

--- app/resource/lastfm_api.py
+++ app/resource/lastfm_api.py
@@ -6,6 +6,7 @@ from app.enum.global_configs import SysConfigs
 from app.enum.default_args import DefaultArgs
 from app.models.current import CurrentModel
 
+
 class LastFM:
     def __init__(self):
         self.api_key = SysConfigs.LASTFM_TOKEN.value
@@ -25,9 +26,9 @@ class LastFM:
         if 'error' in data:
             raise ValueError(data['message'])
 
-        return CurrentModel(user= user_id, 
-                                  artist= data['recenttracks']['track'][0]['artist']['#text'],
-                                  song= data['recenttracks']['track'][0]['name'],
-                                  song_url = data['recenttracks']['track'][0]['url'],
-                                  album = data['recenttracks']['track'][0]['album']['#text'],
-                                  album_cover = data['recenttracks']['track'][0]['image'])
+        return CurrentModel(user=user_id,
+                            artist=data['recenttracks']['track'][0]['artist']['#text'],
+                            song=data['recenttracks']['track'][0]['name'],
+                            song_url=data['recenttracks']['track'][0]['url'],
+                            album=data['recenttracks']['track'][0]['album']['#text'],
+                            album_cover=data['recenttracks']['track'][0]['image'])
